00b70116b64ea7771bb7d457c6f9db73d24d7a78
Mohit Tyagi <mohit.tyagi.de@gmail.com>
2025-09-02 21:12:12 +0200

    added keyvault for azure

--- ShopEasy.API/Program.cs
+++ ShopEasy.API/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ShopEasy.API.Data;
@@ -6,17 +7,29 @@ using ShopEasy.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
+
+var keyVaultEndpoint = builder.Configuration["AzureKeyVault:Endpoint"];
+if (!string.IsNullOrEmpty(keyVaultEndpoint))
 {
-    serverOptions.ListenAnyIP(80); // Container will listen on port 80
-});
+    // Use DefaultAzureCredential which works locally (with az login) and on Azure (with Managed Identity)
+    builder.Configuration.AddAzureKeyVault(new Uri(keyVaultEndpoint), new DefaultAzureCredential());
+}
+
+
+//builder.WebHost.ConfigureKestrel(serverOptions =>
+//{
+//    serverOptions.ListenAnyIP(80); // Container will listen on port 80
+//});
 
 
 // 1.  Custom Services 
 builder.Services.AddAppService();
 
-// 2. Application DB Services
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+// 2. Application DB Services - Now this will get the actual connection string from Key Vault or local config
+var connectionString = builder.Configuration["DbConnection"] ??
+                      builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // 3. Identity Services
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().
@@ -45,6 +58,7 @@ if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
